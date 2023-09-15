b1620682fb7aa26bc1dffdd55362e93057509d84
Ahmed Mohamed <a.mohamed00019@gmail.com>
2023-09-15 22:25:30 +0300

    footer-links

--- components/footer.js
+++ components/footer.js
@@ -3,6 +3,9 @@ import Image from "next/image";
 import Logo from "../css/svg/logo";
 import Insta from "../css/svg/insta";
 import Vector from "../css/svg/Vector";
+import Mail from "../css/svg/mail";
+import Call from "../css/svg/call";
+import Whatsapp from "../css/svg/whatsapp";
 
 import Snap from "../css/svg/snap";
 import Tik from "../css/svg/tik";
@@ -10,42 +13,54 @@ import Tik from "../css/svg/tik";
 const Footer = () => {
   return (
     <>
-    <Container className=" px-20">
-    <div className=" pt-14  gap-4 grid grid-cols-2 text-center w-full">
-   
-          <div className="flex flex-row gap- justify-start">
-            <p>
-
-          بسيارتك.
-            </p>
-            <p>
-
-          بسيارتك.
-            </p>
-            <p>
-
-          بسيارتك.
-            </p>
-            <p>
-
-          بسيارتك.
-            </p>
+      <Container className=" px-20">
+        <div className=" pt-14 gap-5  sm:gap-0 grid grid-cols-1 sm:grid-cols-3 text-center w-full">
+          <div className="flex flex-col gap-5 justify-end items-center sm:items-end order-3 sm:order-1">
+            <h2 className="text-lg	 font-bold  p-0 m-0 text-gray-800 lg:leading-tight md:text-xl ">
+              الدعم والمساعدة
+            </h2>
+        
+            <div className="flex items-center justify-center">
+              <h2 className="text-sm px-2 text-gray-500 font-semibold  p-0 m-0 lg:leading-tight md:text-md ">
+              أبدء محادثة
+              </h2>
+              <Whatsapp />
+            </div>
+            <div className="flex items-center justify-center">
+              <h2 className="text-sm px-2 text-gray-500 font-semibold  p-0 m-0 lg:leading-tight md:text-md ">
+              +966 55584652
+              </h2>
+              <Call />
+            </div>
+            <div className="flex items-center justify-center">
+              <h2 className="text-sm px-2 text-gray-500 font-semibold  p-0 m-0 lg:leading-tight md:text-md ">
+              admin@quickyclean.com.sa
+              </h2>
+              <Mail />
+            </div>
           </div>
-          <div className="grid gap-9 justify-items-end text-right  ">
-            <Logo className=" max-h-10"/>
+          <div className="grid gap-9 justify-items-end text-right  order-2"></div>
+          <div className="grid gap-9 justify-items-center sm:justify-items-end text-right order-1 sm:order-3">
+            <Logo className=" max-h-10" />
             <p className="text-sm  text-gray-500 font-semibold">
-             لإننا نهتم براحتك وأمان سيارتك.، قُمنا بتطوير نظام آمن وموثوق للتحقق من خلفيات فريقنا، حتى تكون على ثقة تامة في اختيارنا للعناية بسيارتك
-              </p>
-              <div className="flex flex-row-reverse items-center gap-3">
-              <Insta/>
-              <Vector/>
-              <Tik/>
-              <Snap/>
-
-              </div>
+              لإننا نهتم براحتك وأمان سيارتك.، قُمنا بتطوير نظام آمن وموثوق
+              للتحقق من خلفيات فريقنا، حتى تكون على ثقة تامة في اختيارنا للعناية
+              بسيارتك
+            </p>
+            <div className="flex flex-row-reverse items-center   gap-3">
+              <Insta />
+              <Vector />
+              <Tik />
+              <Snap />
+            </div>
           </div>
-     </div>
-    </Container>
+        </div>
+          <div className=" pt-14 gap-5  sm:gap-0 grid grid-cols-1 text-center w-full">
+          <h2 className="text-sm px-2 text-gray-500 font-semibold  p-0 m-0 lg:leading-tight md:text-md ">
+          © جميع الحقوق محفوظة ، كويكى كلين 2023
+              </h2>
+          </div>
+      </Container>
     </>
   );
 };
---
+++ css/svg/mail.js
@@ -0,0 +1,26 @@
+
+
+
+const Mail = ({ props }) => (
+    <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g filter="url(#filter0_d_3188_1220)">
+<path d="M27.5 12.5C28.2031 12.526 28.7891 12.7734 29.2578 13.2422C29.7266 13.7109 29.974 14.2969 30 15V25C29.974 25.7031 29.7266 26.2891 29.2578 26.7578C28.7891 27.2266 28.2031 27.474 27.5 27.5H12.5C11.7969 27.474 11.2109 27.2266 10.7422 26.7578C10.2734 26.2891 10.026 25.7031 10 25V15C10.026 14.2969 10.2734 13.7109 10.7422 13.2422C11.2109 12.7734 11.7969 12.526 12.5 12.5H27.5ZM12.5 14.375C12.1094 14.401 11.901 14.6094 11.875 15V15.8594L18.3984 21.25C18.8672 21.6406 19.401 21.8359 20 21.8359C20.599 21.8359 21.1328 21.6406 21.6016 21.25L28.125 15.8594V15C28.099 14.6094 27.8906 14.401 27.5 14.375H12.5ZM27.5 25.625C27.8906 25.599 28.099 25.3906 28.125 25V18.3203L22.8125 22.7344C21.9792 23.4115 21.0417 23.75 20 23.75C18.9583 23.75 18.0208 23.4115 17.1875 22.7344L11.875 18.3203V25C11.901 25.3906 12.1094 25.599 12.5 25.625H27.5Z" fill="#3E3D3E"/>
+</g>
+<defs>
+<filter id="filter0_d_3188_1220" x="-2" y="-1" width="44" height="44" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+<feFlood flood-opacity="0" result="BackgroundImageFix"/>
+<feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+<feOffset dy="1"/>
+<feGaussianBlur stdDeviation="1"/>
+<feComposite in2="hardAlpha" operator="out"/>
+<feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.15 0"/>
+<feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_3188_1220"/>
+<feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_3188_1220" result="shape"/>
+</filter>
+</defs>
+</svg>
+
+  );
+  
+  export default Mail;
+  
