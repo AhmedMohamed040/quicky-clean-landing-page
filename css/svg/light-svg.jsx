4891b2b4157a8982bae57ec703b1457a2574297e
Ahmed Mohamed <a.mohamed00019@gmail.com>
2023-09-22 20:51:12 +0300

    svg-for-hero

---
+++ css/svg/light-svg.jsx
@@ -0,0 +1,330 @@
+import * as React from "react";
+const LightSVG = (props) => (
+  <svg
+    width={1040}
+    height={413}
+    viewBox="0 0 1040 413"
+    fill="none"
+    style={{
+      
+    }}
+    xmlns="http://www.w3.org/2000/svg"
+    xmlnsXlink="http://www.w3.org/1999/xlink"
+    {...props}
+  >
+    <g filter="url(#filter0_i_3653_32)">
+      <ellipse
+        cx={146.5}
+        cy={313.5}
+        rx={107.245}
+        ry={107.245}
+        transform="rotate(-150 146.5 313.5)"
+        fill="url(#paint0_radial_3653_32)"
+      />
+      <ellipse
+        cx={146.5}
+        cy={313.5}
+        rx={107.245}
+        ry={107.245}
+        transform="rotate(-150 146.5 313.5)"
+        fill="#FFCC66"
+        fillOpacity={0.42}
+        style={{ mixBlendMode: "lighten" }}
+      />
+    </g>
+    <g clipPath="url(#clip0_3653_32)">
+      <rect
+        x={160.137}
+        y={64.9408}
+        width={248.89}
+        height={537.144}
+        fill="url(#pattern0)"
+      />
+      <rect
+        x={144}
+        y={47.8549}
+        width={281.383}
+        height={570.32}
+        fill="url(#pattern1)"
+      />
+      <rect x={622} y={62} width={249} height={588} fill="url(#pattern2)" />
+      <rect
+        x={606.358}
+        y={47.8549}
+        width={281.383}
+        height={570.32}
+        fill="url(#pattern3)"
+      />
+      <rect
+        x={374.592}
+        y={18.184}
+        width={269.924}
+        height={582.541}
+        fill="url(#pattern4)"
+      />
+      <rect
+        x={357.091}
+        y={-0.345886}
+        width={305.164}
+        height={618.521}
+        fill="url(#pattern5)"
+      />
+    </g>
+    <g filter="url(#filter1_i_3653_32)">
+      <ellipse
+        cx={365.724}
+        cy={83.7239}
+        rx={44.8799}
+        ry={44.8799}
+        transform="rotate(-173.571 365.724 83.7239)"
+        fill="url(#paint1_radial_3653_32)"
+      />
+      <ellipse
+        cx={365.724}
+        cy={83.7239}
+        rx={44.8799}
+        ry={44.8799}
+        transform="rotate(-173.571 365.724 83.7239)"
+        fill="#FFCC66"
+        fillOpacity={0.42}
+        style={{ mixBlendMode: "lighten" }}
+      />
+    </g>
+    <g filter="url(#filter2_i_3653_32)">
+      <ellipse
+        cx={107.245}
+        cy={107.245}
+        rx={107.245}
+        ry={107.245}
+        transform="matrix(0.866025 -0.5 -0.5 -0.866025 854.245 452)"
+        fill="url(#paint2_radial_3653_32)"
+      />
+      <ellipse
+        cx={107.245}
+        cy={107.245}
+        rx={107.245}
+        ry={107.245}
+        transform="matrix(0.866025 -0.5 -0.5 -0.866025 854.245 452)"
+        fill="#FFCC66"
+        fillOpacity={0.42}
+        style={{ mixBlendMode: "lighten" }}
+      />
+    </g>
+    <defs>
+      <filter
+        id="filter0_i_3653_32"
+        x={39.2366}
+        y={206.237}
+        width={214.527}
+        height={217.384}
+        filterUnits="userSpaceOnUse"
+        colorInterpolationFilters="sRGB"
+      >
+        <feFlood floodOpacity={0} result="BackgroundImageFix" />
+        <feBlend
+          mode="normal"
+          in="SourceGraphic"
+          in2="BackgroundImageFix"
+          result="shape"
+        />
+        <feColorMatrix
+          in="SourceAlpha"
+          type="matrix"
+          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+          result="hardAlpha"
+        />
+        <feOffset dy={8} />
+        <feGaussianBlur stdDeviation={1.42857} />
+        <feComposite in2="hardAlpha" operator="arithmetic" k2={-1} k3={1} />
+        <feColorMatrix
+          type="matrix"
+          values="0 0 0 0 1 0 0 0 0 1 0 0 0 0 1 0 0 0 0.74 0"
+        />
+        <feBlend
+          mode="normal"
+          in2="shape"
+          result="effect1_innerShadow_3653_32"
+        />
+      </filter>
+      <pattern
+        id="pattern0"
+        patternContentUnits="objectBoundingBox"
+        width={1}
+        height={1}
+      >
+        <use
+          xlinkHref="#image0_3653_32"
+          transform="matrix(0.000666667 0 0 0.000308905 0 -0.00166107)"
+        />
+      </pattern>
+      <pattern
+        id="pattern1"
+        patternContentUnits="objectBoundingBox"
+        width={1}
+        height={1}
+      >
+        <use
+          xlinkHref="#image1_3653_32"
+          transform="scale(0.000745712 0.000367918)"
+        />
+      </pattern>
+      <pattern
+        id="pattern2"
+        patternContentUnits="objectBoundingBox"
+        width={1}
+        height={1}
+      >
+        <use
+          xlinkHref="#image2_3653_32"
+          transform="matrix(0.000666371 0 0 0.000282188 0.00198989 0.00215331)"
+        />
+      </pattern>
+      <pattern
+        id="pattern3"
+        patternContentUnits="objectBoundingBox"
+        width={1}
+        height={1}
+      >
+        <use
+          xlinkHref="#image1_3653_32"
+          transform="scale(0.000745712 0.000367918)"
+        />
+      </pattern>
+      <pattern
+        id="pattern4"
+        patternContentUnits="objectBoundingBox"
+        width={1}
+        height={1}
+      >
+        <use
+          xlinkHref="#image3_3653_32"
+          transform="matrix(0.000666667 0 0 0.000308905 0 -0.00166115)"
+        />
+      </pattern>
+      <pattern
+        id="pattern5"
+        patternContentUnits="objectBoundingBox"
+        width={1}
+        height={1}
+      >
+        <use
+          xlinkHref="#image1_3653_32"
+          transform="scale(0.000745712 0.000367918)"
+        />
+      </pattern>
+      <filter
+        id="filter1_i_3653_32"
+        x={320.84}
+        y={38.8399}
+        width={89.7679}
+        height={92.6251}
+        filterUnits="userSpaceOnUse"
+        colorInterpolationFilters="sRGB"
+      >
+        <feFlood floodOpacity={0} result="BackgroundImageFix" />
+        <feBlend
+          mode="normal"
+          in="SourceGraphic"
+          in2="BackgroundImageFix"
+          result="shape"
+        />
+        <feColorMatrix
+          in="SourceAlpha"
+          type="matrix"
+          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+          result="hardAlpha"
+        />
+        <feOffset dy={8} />
+        <feGaussianBlur stdDeviation={1.42857} />
+        <feComposite in2="hardAlpha" operator="arithmetic" k2={-1} k3={1} />
+        <feColorMatrix
+          type="matrix"
+          values="0 0 0 0 1 0 0 0 0 1 0 0 0 0 1 0 0 0 0.74 0"
+        />
+        <feBlend
+          mode="normal"
+          in2="shape"
+          result="effect1_innerShadow_3653_32"
+        />
+      </filter>
+      <filter
+        id="filter2_i_3653_32"
+        x={786.237}
+        y={198.237}
+        width={214.527}
+        height={217.384}
+        filterUnits="userSpaceOnUse"
+        colorInterpolationFilters="sRGB"
+      >
+        <feFlood floodOpacity={0} result="BackgroundImageFix" />
+        <feBlend
+          mode="normal"
+          in="SourceGraphic"
+          in2="BackgroundImageFix"
+          result="shape"
+        />
+        <feColorMatrix
+          in="SourceAlpha"
+          type="matrix"
+          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+          result="hardAlpha"
+        />
+        <feOffset dy={8} />
+        <feGaussianBlur stdDeviation={1.42857} />
+        <feComposite in2="hardAlpha" operator="arithmetic" k2={-1} k3={1} />
+        <feColorMatrix
+          type="matrix"
+          values="0 0 0 0 1 0 0 0 0 1 0 0 0 0 1 0 0 0 0.74 0"
+        />
+        <feBlend
+          mode="normal"
+          in2="shape"
+          result="effect1_innerShadow_3653_32"
+        />
+      </filter>
+      <radialGradient
+        id="paint0_radial_3653_32"
+        cx={0}
+        cy={0}
+        r={1}
+        gradientUnits="userSpaceOnUse"
+        gradientTransform="translate(146.5 313.5) rotate(90) scale(107.245 107.245)"
+      >
+        <stop stopColor="white" />
+        <stop offset={1} stopColor="white" stopOpacity={0} />
+      </radialGradient>
+      <radialGradient
+        id="paint1_radial_3653_32"
+        cx={0}
+        cy={0}
+        r={1}
+        gradientUnits="userSpaceOnUse"
+        gradientTransform="translate(365.724 83.7239) rotate(90) scale(44.8799 44.8799)"
+      >
+        <stop stopColor="white" />
+        <stop offset={1} stopColor="white" stopOpacity={0} />
+      </radialGradient>
+      <radialGradient
+        id="paint2_radial_3653_32"
+        cx={0}
+        cy={0}
+        r={1}
+        gradientUnits="userSpaceOnUse"
+        gradientTransform="translate(107.245 107.245) rotate(90) scale(107.245 107.245)"
+      >
+        <stop stopColor="white" />
+        <stop offset={1} stopColor="white" stopOpacity={0} />
+      </radialGradient>
+      <clipPath id="clip0_3653_32">
+        <rect
+          width={744}
+          height={413}
+          fill="white"
+          transform="translate(144)"
+        />
+      </clipPath>
+    
+    </defs>
+  </svg>
+);
+export default LightSVG;
