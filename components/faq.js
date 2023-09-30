67b592f465bedd38c9049abbec1669d8c14c72b7
Ahmed Mohamed <a.mohamed00019@gmail.com>
2023-09-30 21:19:22 +0300

    change-text

--- components/faq.js
+++ components/faq.js
@@ -45,7 +45,7 @@ const faqdata = [
   {
     question: "What is your refund policy ? ",
     answer:
-      "If you're unhappy with your purchase for any reason, email us within 90 days and we'll refund you in full, no questions asked.",
+      "If you are unhappy with your purchase for any reason, email us within 90 days and we'll refund you in full, no questions asked.",
   },
   {
     question: "Do you offer technical support? ",
